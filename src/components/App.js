117861561c02428279915f21e3c345c9a6230eae
VipulTrimbake610 <vipultrimbake610@gmail.com>
2023-08-29 20:20:53 +0530

    submitting code-Tue Aug 29 2023 20:20:50 GMT+0530 (India Standard Time)

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,18 @@
 
-import React from "react";
+import React, { useState } from "react";
 import './../styles/App.css';
 
 const App = () => {
+  let [sum, setSum] = useState(0);
+  function handleChange(e){
+    setSum(sum+parseInt(e.target.value));
+  }
   return (
     <div>
         {/* Do not remove the main div */}
+        <h1>Sum Calculator</h1>
+        <input type="number" name="" id="" onChange={handleChange} />
+        <p>Sum: {sum}</p>
     </div>
   )
 }
